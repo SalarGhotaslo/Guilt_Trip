19d6cf0e5b0bdbdff2efd12d476bc06b2a12c358
Hamish <hamisharro@icloud.com>
2021-03-16 12:47:10 +0000

    broken splash screen

--- App.js
+++ App.js
@@ -2,23 +2,63 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { performStepAPI } from './stepApi'
+import * as SplashScreen from 'expo-splash-screen';
 
-performStepAPI()
+export default class App extends React.Component {
+  state = {
+    appIsReady: false,
+  };
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Guilt Trip</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  async componentDidMount() {
+    // Prevent native splash screen from autohiding
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+    this.prepareResources();
+  }
+
+  /**
+   * Method that serves to load resources and make API calls
+   */
+  prepareResources = async () => {
+    try {
+      stepCount = await performStepAPI();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      this.setState({ appIsReady: true }, async () => {
+        await SplashScreen.hideAsync();
+      });
+    }
+  };
+
+  render() {
+    if (!this.state.appIsReady) {
+      return null;
+    }
+    console.log(stepCount);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Guilt trip demo! 👋</Text>
+        <Text>{stepCount}</Text>
+      </View>
+    );
+  }
 }
+
+// Put any code you need to prepare your app in these functions
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#aabbcc',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
--- stepApi.js
+++ stepApi.js
@@ -1,11 +1,10 @@
 import { Pedometer } from 'expo-sensors';
 
-export function performStepAPI() {
+export async function performStepAPI() {
   Pedometer.isAvailableAsync().then(function(isAvailable) {
     if (isAvailable) {
       let start = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), end = new Date;
       Pedometer.getStepCountAsync(start, end).then(function(response) {
-        console.log(response.steps);
         return response.steps;
       });
     }
