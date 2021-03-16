c4633ae58ff3f6fa98476eb03e5d90a85dfa4399
Reeshul <reeshul@hotmail.com>
2021-03-16 19:00:51 +0000

    refactored with app.js with hooks

--- App.js
+++ App.js
@@ -1,76 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Pedometer } from "expo-sensors";
 
-export default class App extends React.Component {
-  state = {
-    isPedometerAvailable: "checking",
-    pastStepCount: 0,
-    currentStepCount: 0,
-  };
+export default function App() {
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState("checking");
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
 
-  componentDidMount() {
-    this._subscribe();
-  }
-
-  componentWillUnmount() {
-    this._unsubscribe();
-  }
-
-  _subscribe = () => {
-    this._subscription = Pedometer.watchStepCount((result) => {
-      this.setState({
-        currentStepCount: result.steps,
-      });
+  useEffect(() => {
+    sessionStepCount = Pedometer.watchStepCount((result) => {
+      setCurrentStepCount(result.steps);
     });
 
     Pedometer.isAvailableAsync().then(
       (result) => {
-        this.setState({
-          isPedometerAvailable: String(result),
-        });
+        setIsPedometerAvailable(result.toString());
       },
       (error) => {
-        this.setState({
-          isPedometerAvailable: "Could not get isPedometerAvailable: " + error,
-        });
+        setIsPedometerAvailable(`Could not get isPedometerAvailable: ${error}`);
       }
     );
 
     const end = new Date();
     const start = new Date();
     start.setHours(0, 0, 0, 0);
-    console.log(start);
-    console.log(end);
     Pedometer.getStepCountAsync(start, end).then(
       (result) => {
-        this.setState({ pastStepCount: result.steps });
+        setPastStepCount(result.steps);
       },
       (error) => {
-        this.setState({
-          pastStepCount: "Could not get stepCount: " + error,
-        });
+        setPastStepCount(`Could not get stepCount: ${error}`);
       }
     );
-  };
 
-  _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
-  };
+    return function resetCurrentSession() {
+      sessionStepCount && sessionStepCount.remove();
+      sessionStepCount = null;
+    };
+  }, []);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {/* <Text>Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}</Text> */}
-        <Text>
-          Steps taken today:{" "}
-          {this.state.pastStepCount + this.state.currentStepCount}
-        </Text>
-        {/* <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text> */}
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      {/* <Text>Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}</Text> */}
+      {isPedometerAvailable && (
+        <Text>Steps taken today: {pastStepCount + currentStepCount}</Text>
+      )}
+      {/* <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text> */}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
