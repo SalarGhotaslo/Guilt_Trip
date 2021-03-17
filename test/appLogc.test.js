aa0e45a99f7b4d446db99b986139da4d2c476ba5
Reeshul <reeshul@hotmail.com>
2021-03-17 12:22:27 +0000

    added app logic for decreasing colony population is target is not met

--- test/appLogc.test.js
+++ test/appLogc.test.js
@@ -11,6 +11,13 @@ describe("AppLogic function", () => {
   test("if steps target is reached, colony population increases", () => {
     testSteps = 5000;
     AppLogic(testTarget, testColony, testSteps);
+    expect(testColony.showPopulation()).not.toBe(5);
     expect(testColony.showPopulation()).toBe(6);
+  });
+  test("if steps target is not reached, colony population decreases", () => {
+    testSteps = 4999;
+    AppLogic(testTarget, testColony, testSteps);
+    expect(testColony.showPopulation()).not.toBe(5);
+    expect(testColony.showPopulation()).toBe(4);
   });
 });
