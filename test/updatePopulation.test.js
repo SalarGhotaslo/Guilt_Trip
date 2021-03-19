e94540973b5e3a06a12702ecec21988f95331f34
PetarSimonovic <petar@simonovic.com>
2021-03-19 08:16:44 +0000

    updatePopulation: tests for non-consecutive results and game over

--- test/updatePopulation.test.js
+++ test/updatePopulation.test.js
@@ -7,6 +7,10 @@ import { performStepApi } from "../src/performStepApi";
 Pedometer.getStepCountAsync = jest.fn();
 Pedometer.isAvailableAsync = jest.fn();
 
+beforeEach(() => {
+  Pedometer.isAvailableAsync.mockReturnValue(true);
+});
+
 
 describe("updatePopulation function", () => {
   let testTarget, testColony, testSteps;
@@ -21,7 +25,6 @@ describe("updatePopulation function", () => {
     let yesterday = new Date();
     yesterday.setHours(0, 0, 0, 0);
     yesterday.setDate(yesterday.getDate() - 1);
-    Pedometer.isAvailableAsync.mockReturnValue(true)
     Pedometer.getStepCountAsync.mockReturnValue({"steps":50000})
     await updatePopulation(testTarget, testColony, yesterday, today)
     console.log(testColony.showPopulation())
@@ -35,7 +38,6 @@ describe("updatePopulation function", () => {
     let yesterday = new Date();
     yesterday.setHours(0, 0, 0, 0);
     yesterday.setDate(yesterday.getDate() - 1);
-    Pedometer.isAvailableAsync.mockReturnValue(true)
     Pedometer.getStepCountAsync.mockReturnValue({"steps":5})
     await updatePopulation(testTarget, testColony, yesterday, today)
     console.log(testColony.showPopulation())
@@ -50,7 +52,6 @@ describe("updatePopulation function", () => {
     threeDaysAgo.setHours(0, 0, 0, 0);
     threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
     console.log(threeDaysAgo)
-    Pedometer.isAvailableAsync.mockReturnValue(true)
     Pedometer.getStepCountAsync.mockReturnValue({"steps":50000})
     await updatePopulation(testTarget, testColony, threeDaysAgo, today);
     console.log("Population is now")
@@ -66,7 +67,6 @@ describe("updatePopulation function", () => {
     threeDaysAgo.setHours(0, 0, 0, 0);
     threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
     console.log(threeDaysAgo)
-    Pedometer.isAvailableAsync.mockReturnValue(true)
     Pedometer.getStepCountAsync.mockReturnValue({"steps":5})
     await updatePopulation(testTarget, testColony, threeDaysAgo, today);
     console.log("Population is now")
@@ -74,5 +74,66 @@ describe("updatePopulation function", () => {
     expect(testColony.showPopulation()).not.toBe(5);
     expect(testColony.showPopulation()).toBe(2);
   });
+
+  test("it decreases by 2 and increases by 1 if the target is: missed, missed, hit", async () => {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    let threeDaysAgo = new Date();
+    threeDaysAgo.setHours(0, 0, 0, 0);
+    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+    console.log(threeDaysAgo)
+    Pedometer.getStepCountAsync.mockReturnValueOnce({"steps":5}).mockReturnValueOnce({"steps":5}).mockReturnValue({"steps":5000});
+    await updatePopulation(testTarget, testColony, threeDaysAgo, today);
+    console.log("Population is now")
+    console.log(testColony.showPopulation())
+    expect(testColony.showPopulation()).not.toBe(5);
+    expect(testColony.showPopulation()).toBe(4);
+  });
+
+  test("it increases by 2 and decreases by 1 if the target is: hit, hit, missed", async () => {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    let threeDaysAgo = new Date();
+    threeDaysAgo.setHours(0, 0, 0, 0);
+    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+    console.log(threeDaysAgo)
+    Pedometer.getStepCountAsync.mockReturnValueOnce({"steps":5000}).mockReturnValueOnce({"steps":5000}).mockReturnValue({"steps":5});
+    await updatePopulation(testTarget, testColony, threeDaysAgo, today);
+    console.log("Population is now")
+    console.log(testColony.showPopulation())
+    expect(testColony.showPopulation()).not.toBe(5);
+    expect(testColony.showPopulation()).toBe(6);
+  });
+
+  test("it increases by 3 and decreases by 1 if the target is: hit, missed, hit, hit", async () => {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    let fourDaysAgo = new Date();
+    fourDaysAgo.setHours(0, 0, 0, 0);
+    fourDaysAgo.setDate(fourDaysAgo.getDate() - 4);
+    console.log(fourDaysAgo)
+    Pedometer.getStepCountAsync.mockReturnValueOnce({"steps":5000}).mockReturnValueOnce({"steps":5}).mockReturnValue({"steps":7000}).mockReturnValue({"steps":6000});
+    await updatePopulation(testTarget, testColony, fourDaysAgo, today);
+    console.log("Population is now")
+    console.log(testColony.showPopulation())
+    expect(testColony.showPopulation()).not.toBe(5);
+    expect(testColony.showPopulation()).toBe(7);
+  });
+
+  test("it resets the colony to 5 if game is over (5 consecutive missed dauys)", async () => {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    let fiveDaysAgo = new Date();
+    fiveDaysAgo.setHours(0, 0, 0, 0);
+    fiveDaysAgo.setDate(fiveDaysAgo.getDate() - 5);
+    Pedometer.getStepCountAsync.mockReturnValue({"steps":5})
+    await updatePopulation(testTarget, testColony, fiveDaysAgo, today);
+    console.log("Population is now")
+    console.log(testColony.showPopulation())
+    expect(testColony.showPopulation()).toBe(0);
+    expect(Colony).toHaveBeenCalled
+  });
+
+
 
 });
