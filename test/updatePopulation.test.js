cc8823cea81849df0cca42bfb4ab891ea5e4c589
Reeshul <reeshul@hotmail.com>
2021-03-19 02:24:32 +0000

    fixed test

--- test/updatePopulation.test.js
+++ test/updatePopulation.test.js
@@ -3,24 +3,32 @@ import { Target } from "../src/Target";
 import { Colony } from "../src/Colony";
 
 describe("updatePopulation function", () => {
-  let testTarget, testColony, testSteps;
+  let testTarget, testColony, threeDaysAgo, today;
   beforeEach(() => {
     testTarget = new Target();
     testColony = new Colony();
+    today = new Date();
   });
 
-  xtest("it increase by 3 when target is met 3 days in a row", async () => {
-    let today = new Date();
+  test("it increase by 3 when target is met 3 days in a row", async () => {
     today.setHours(0, 0, 0, 0);
-    let threeDaysAgo = new Date();
+    threeDaysAgo = new Date();
     threeDaysAgo.setHours(0, 0, 0, 0);
     threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
-    console.log(threeDaysAgo);
-    // const performStepApiMock = jest.fn().mockReturnValue(5000);
-    // updatePopulation.performStepApi = performStepApiMock
-    updatePopulation(testTarget, testColony, threeDaysAgo, today);
+    const mockGetStepCountFn = jest.fn().mockResolvedValue(5000);
+    const resolvedStepCount = await mockGetStepCountFn();
+    const mockUpdatePopFn = jest.fn((target, colony, lastLogin, today) => {
+      while (today.getTime() > lastLogin.getTime()) {
+        const range = lastLogin;
+        range.setDate(range.getDate() + 1);
+        target.isReached(resolvedStepCount)
+          ? colony.addCreature()
+          : colony.killCreature();
+        lastLogin = range;
+      }
+    });
+    mockUpdatePopFn(testTarget, testColony, threeDaysAgo, today);
     expect(testColony.showPopulation()).not.toBe(5);
     expect(testColony.showPopulation()).toBe(8);
-
   });
 });
