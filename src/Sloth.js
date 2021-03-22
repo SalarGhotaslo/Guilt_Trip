3706dbc01d7a5d3a55def00782d510712d15180a
bateso88 <bateso@hotmail.co.uk>
2021-03-22 14:21:15 +0000

    add random name generator

--- src/Sloth.js
+++ src/Sloth.js
@@ -1,7 +1,13 @@
+import { names } from "../src/names";
+
 export class Sloth {
-  constructor(name, personality, passion) {
+  constructor(name = this._randomName(), personality, passion) {
     this.name = name;
     this.personality = personality;
     this.passion = passion;
+  }
+
+  _randomName() {
+    return names[Math.floor(Math.random() * names.length)]
   }
 }
--- test/sloth.test.js
+++ test/sloth.test.js
@@ -1,3 +1,4 @@
+import { names } from "../src/names";
 import { Sloth } from "../src/Sloth";
 
 describe("Sloth", () => {
@@ -14,5 +15,15 @@ describe("Sloth", () => {
   test("has something it likes", () => {
     let sloth = new Sloth("Jimmy", "joyful", "javascript")
     expect(sloth.passion).toEqual("javascript")
+  });
+
+  test("names", () => {
+    expect(names.length).toEqual(77407)
+  });
+
+  test("random name is generated when default aloth created", () => {
+    sloth = new Sloth();
+    console.log(sloth.name)
+    expect(names.includes(sloth.name)).toEqual(true)
   });
 });
