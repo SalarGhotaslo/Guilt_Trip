6cf17a22112a199823acebc1551dc2180c502aca
Dan T <dp.tyler@hotmail.co.uk>
2021-03-22 11:01:30 +0000

    debugging attempt 1

--- src/updatePopulation.js
+++ src/updatePopulation.js
@@ -3,21 +3,24 @@ import { performStepApi } from "./performStepApi";
 
 export async function updatePopulation(target, colony, lastLogin, today) {
   while (today.getTime() > lastLogin.getTime()) {
-    console.log(`lastLogin: ${lastLogin}`)
+    console.log(`lastLogin: ${lastLogin}`);
     let range = JSON.stringify(lastLogin);
-    console.log(`range: ${range}`)
-    range = JSON.parse(range)
-    range.setDate(range.getDate() + 1);
-    let steps = await performStepApi(lastLogin, range);
-    console.log(`steps: ${steps}`)
+    startRange = new Date(range);
+    console.log(`range1: ${startRange}`);
+    startRange.setDate(range.getDate() + 1);
+    console.log(`range2: ${startRange}`);
+    let steps = await performStepApi(lastLogin, startRange);
+    console.log(`steps: ${steps}`);
     target.isReached(steps) ? colony.addCreature() : colony.killCreature();
-    lastLogin = range;
+    lastLogin = startRange;
   }
-  if(colony.showPopulation() === 0 ){
-    console.log("your population died you negligent lazy bastard")
+  if (colony.showPopulation() === 0) {
+    console.log("your population died you negligent lazy bastard");
     return new Colony();
   } else {
-    console.log(`Welcome back! Your population is now ${colony.showPopulation()}`)
+    console.log(
+      `Welcome back! Your population is now ${colony.showPopulation()}`
+    );
     return colony;
   }
 }
