c35767f63cf230b18ac4d92cb39070083a4b7beb
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-22 11:34:10 +0000

    bug fixed by Ollie - changed to a string

--- src/updatePopulation.js
+++ src/updatePopulation.js
@@ -4,15 +4,14 @@ import { performStepApi } from "./performStepApi";
 export async function updatePopulation(target, colony, lastLogin, today) {
   while (today.getTime() > lastLogin.getTime()) {
     console.log(`lastLogin: ${lastLogin}`);
-    let range = JSON.stringify(lastLogin);
-    startRange = new Date(range);
-    console.log(`range1: ${startRange}`);
-    startRange.setDate(range.getDate() + 1);
-    console.log(`range2: ${startRange}`);
-    let steps = await performStepApi(lastLogin, startRange);
+    let range = String(lastLogin);
+    range = new Date(range);
+    range.setDate(range.getDate() + 1);
+    console.log(`range: ${range}`);
+    let steps = await performStepApi(lastLogin, range);
     console.log(`steps: ${steps}`);
     target.isReached(steps) ? colony.addCreature() : colony.killCreature();
-    lastLogin = startRange;
+    lastLogin = range;
   }
   if (colony.showPopulation() === 0) {
     console.log("your population died you negligent lazy bastard");
