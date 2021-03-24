56c264cfd01969a4e44aec5a226acd486801c330
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-24 17:13:50 +0000

    imported sloth svgs into svgLoader

--- src/svgLoader.js
+++ src/svgLoader.js
@@ -1,11 +1,25 @@
-import React, { Component } from "react";
+// Regular Sloths
 
-import BigBallsTom from "../assets/svgs/segments/regular/BigBallsTom";
-import Sarah from "../assets/svgs/segments/regular/Sarah";
-import YPatrick from "../assets/svgs/segments/regular/YPatrick";
-import HiddenSteve from "../assets/svgs/segments/regular/HiddenSteve";
-import Patrick from "../assets/svgs/segments/regular/Patrick";
-import Steve from "../assets/svgs/segments/regular/Steve";
+import React, { Component } from "react";
+import TreeSegmentTom from "../assets/svgs/segments/regular/BigBallsTom";
+import TreeSegmentSarah from "../assets/svgs/segments/regular/Sarah";
+import TreeSegmentYPatrick from "../assets/svgs/segments/regular/YPatrick";
+import TreeSegmentHiddenSteve from "../assets/svgs/segments/regular/HiddenSteve";
+
+//Legendary Sloths
+
+import Ash from "../assets/svgs/segments/legendary/Ash";
+import Aviator from "../assets/svgs/segments/legendary/Aviator";
+import BeerGlass from "../assets/svgs/segments/legendary/BeerGlass";
+import BeerHat from "../assets/svgs/segments/legendary/BeerHat";
+import Devil from "../assets/svgs/segments/legendary/Devil";
+import Flowers from "../assets/svgs/segments/legendary/Flowers";
+import HairRibbon from "../assets/svgs/segments/legendary/HairRibbon";
+import HeadPhones from "../assets/svgs/segments/legendary/HeadPhones";
+import Jason from "../assets/svgs/segments/legendary/Jason";
+import Shark from "../assets/svgs/segments/legendary/Shark";
+import thugLife from "../assets/svgs/segments/legendary/thugLife";
+import Witch from "../assets/svgs/segments/legendary/Witch";
 
 export const arrayOfClassics = [
   <YPatrick />,
