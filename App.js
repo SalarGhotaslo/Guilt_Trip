baebbe96cf1b644a35328723d25deeae72a1071e
Reeshul <reeshul@hotmail.com>
2021-03-25 11:46:20 +0000

    added Patrick

--- App.js
+++ App.js
@@ -31,7 +31,12 @@ import { Target, DEFAULT_TARGET } from "./src/Target";
 import { Colony, DEFAULT_POPULATION } from "./src/Colony";
 import { performStepApi, DAY } from "./src/performStepApi";
 import { createColony } from "./src/createColony";
-import { slothSpeech, setXPosition, setYPosition, windowWidth } from "./src/slothSpeech";
+import {
+  slothSpeech,
+  setXPosition,
+  setYPosition,
+  windowWidth,
+} from "./src/slothSpeech";
 import { render } from "react-dom";
 import { alertsFunction } from "./src/alerts";
 import { FancyAlert } from "react-native-expo-fancy-alerts";
@@ -39,6 +44,12 @@ import TreeTop from "./assets/svgs/TreeTop";
 import TreeBottom from "./assets/svgs/TreeBottom";
 import Svg from "react-native-svg";
 import { arrayOfClassics, arrayOfRares } from "./src/svgLoader";
+import * as Font from "expo-font";
+
+let customFonts = {
+  Patrick: require("./assets/fonts/PatrickHand-Regular.ttf"),
+  Karla: require("./assets/fonts/Karla-VariableFont_wght.ttf"),
+};
 
 export default class App extends Component {
   state = {
@@ -56,6 +67,7 @@ export default class App extends Component {
 
   async componentDidMount() {
     try {
+      await Font.loadAsync(customFonts);
       await SplashScreen.preventAutoHideAsync();
     } catch (e) {
       console.warn(e);
@@ -130,7 +142,6 @@ export default class App extends Component {
       save("sloths", JSON.stringify(colony.sloths));
       var steps = await performStepApi();
       var target = new Target();
-
     } catch (e) {
       console.log(e);
     } finally {
@@ -188,12 +199,13 @@ export default class App extends Component {
           slothPopulation={this.state.population}
           slothCollection={this.state.slothCollection}
         />
-
         <TreeBottom
           slothPopulation={this.state.population}
           count={this.state.stepCount + this.state.currentStepCount}
           remaining={
-            this.state.dynamicTarget - this.state.stepCount - this.state.currentStepCount
+            this.state.dynamicTarget -
+            this.state.stepCount -
+            this.state.currentStepCount
           }
           target={this.state.dynamicTarget}
         />
@@ -342,7 +354,7 @@ const SpeechBubble = (props) => {
         left: props.xPosition,
         right: 0,
         bottom: 0,
-        width: 0.35*windowWidth,
+        width: 0.35 * windowWidth,
         height: 70,
         backgroundColor: props.speechBackground,
         justifyContent: "center",
@@ -351,7 +363,9 @@ const SpeechBubble = (props) => {
         padding: 12,
       }}
     >
-      <Text style={{ alignItems: "center", fontSize: 12 }}>
+      <Text
+        style={{ alignItems: "center", fontSize: 14, fontFamily: "Patrick" }}
+      >
         {props.slothWords}
       </Text>
     </View>
