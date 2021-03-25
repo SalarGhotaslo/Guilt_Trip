b52c76ed0e5cf84fd178a59c0a9c75787b2a7886
PetarSimonovic <petar@simonovic.com>
2021-03-25 12:32:02 +0000

    Another typo fixed!

--- src/neutralTalk.js
+++ src/neutralTalk.js
@@ -7,7 +7,7 @@ export let neutralTalk = [
   "Without sloths there would be no avocados.",
   "Sloths have the lowest metabolic rate of any mammal.",
   "Sweatsuits are the best kind of suit.",
-  "Algae on our fur an acts as camouflage.",
+  "Algae on our fur acts as camouflage.",
   "We spend 90% of our lives hanging upside down.",
   "It takes us 30 days to digest one leaf.",
   "Sloths are related to armadillos and anteaters."
